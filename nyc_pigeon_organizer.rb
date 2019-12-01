6948532cae401bad551ba6192d8fc06f09c24c64
Noah Scala <tobi8912@gmail.com>
2019-12-01 19:49:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   final = {}
-  data.each do |color_gender_lives, specifics|
-    
+  data.each do |color_gender_lives, all_other|
+    all_other.each do |category, name|
   end 
   final 
 end
