59b3e1344f9944bfd0e13b800c6e03f7114fbed7
Noah Scala <tobi8912@gmail.com>
2019-12-01 19:52:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,12 +2,15 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   final = {}
   data.each do |color_gender_lives, all_other|
-    all_other.each do |category, name_array|
-      name_array.each do |pigeon_name|
+    all_other.each do |category, name_arrays|
+      name_arrays.each do |pigeon_name|
         final[pigeon_name] = {:color => [], :gender => [], :lives => []}
       end 
     end 
   end
-  
+  pigeon_name_array = final.keys
+  data[:color].each do 
+    
+  end 
   final 
 end
